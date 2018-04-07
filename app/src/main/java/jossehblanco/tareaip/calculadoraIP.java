bd9a47e63a72e6f954f0ad072f568bf0665687c0
jossehblanco <jossehblanco@gmail.com>
2018-04-07 17:50:11 -0600

    Se añadió la capacidad de generar la parte de red, parte de host y numero de hosts

--- app/src/main/java/jossehblanco/tareaip/calculadoraIP.java
+++ app/src/main/java/jossehblanco/tareaip/calculadoraIP.java
@@ -7,18 +7,22 @@ import java.util.Arrays;
  */
 
 public class calculadoraIP {
-    private int[] ipByteArray, maskByteArray = {0,0,0,0};
+    private int[] ipByteArray;
+    private int[] maskByteArray = {0,0,0,0};
     private int maskPrefix;
     public calculadoraIP(String direccionIP, int maskPrefix){
         //Conversion de la ip de string a un arreglo de ints, separados en 4 octetos
-        String[] dir = direccionIP.split(".");
+        ipByteArray = new int[4];
+        String[] dir = direccionIP.split("\\.");
         for(int i = 0; i < 4; i++){
-            this.ipByteArray[i] = Integer.parseInt(dir[0]);
+            ipByteArray[i] = Integer.parseInt(dir[i]);
         }
         this.maskPrefix = maskPrefix;
         //Metodo que convierte el prefijo a una mascara de Red trabajable
         this.generarMascara(this.maskPrefix);
     }
+
+
     private void generarMascara(int maskPrefix){
         int prefijo = maskPrefix;
         int contador = 0, cont_potencia = 7;
@@ -46,8 +50,9 @@ public class calculadoraIP {
 
         return broadcast;
     }
-    public int genHosts(){
-        return (int)(Math.pow(2, 32-maskPrefix)-2);
+    public String genHosts(){
+        if(maskPrefix < 31) return ""+(int)(Math.pow(2, 32-maskPrefix)-2);
+        else return ""+ (int)(Math.pow(2, 32-maskPrefix));
     }
     public String genNetId(){
         String netId ="";
@@ -60,6 +65,29 @@ public class calculadoraIP {
                 netId += (ipByteArray[i]&maskByteArray[i]);
             }
         }
+        netId+=("/"+maskPrefix);
         return netId;
+    }
+
+    public String genRedpt(){
+        String redPt ="";
+        for(int i = 0; i < 4; i++) {
+            if (maskByteArray[i] != 0) {
+                redPt += (ipByteArray[i] & maskByteArray[i]);
+                if(i <3)redPt += ".";
+            }
+        }
+        return redPt;
+    }
+
+    public String genHostpt(){
+        String hostPt ="";
+        for(int i = 0; i < 4; i++) {
+            if(255-maskByteArray[i] != 0){
+                hostPt += ".";
+                hostPt += (ipByteArray[i] & (255-maskByteArray[i]));
+            }
+        }
+        return hostPt;
     }
 }
